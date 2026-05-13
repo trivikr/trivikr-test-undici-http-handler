303ba1c5a7a426e3a5be61dcef8ab07e7fe9540e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2026-05-13 13:20:31 -0700

    test: remove redundant 'constructor and create' tests

--- src/undici-http-handler.spec.ts
+++ src/undici-http-handler.spec.ts
@@ -108,16 +108,6 @@ describe("UndiciHttpHandler", () => {
     handler?.destroy();
   });
 
-  describe("constructor and create", () => {
-    it("creates a new instance with no options", () => {
-      handler = new UndiciHttpHandler();
-    });
-
-    it("creates a new instance with options", () => {
-      handler = new UndiciHttpHandler({ logger: createMockLogger() });
-    });
-  });
-
   describe("handle", () => {
     it("makes a basic GET request", async () => {
       handler = new UndiciHttpHandler();
